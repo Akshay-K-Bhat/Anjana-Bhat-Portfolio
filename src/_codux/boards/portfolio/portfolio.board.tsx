58ab301e70cbbead8ccc898e6a81661c30e24697
Akshay-K-Bhat <akshaybhat1206@gmail.com>
2023-08-03 19:28:47 +0530

    commit

--- src/_codux/boards/portfolio/portfolio.board.tsx
+++ src/_codux/boards/portfolio/portfolio.board.tsx
@@ -2,6 +2,11 @@ import { createBoard } from '@wixc3/react-board';
 import { Portofolio } from '../../../components/portofolio/portofolio';
 
 export default createBoard({
-    name: 'Portfolio',
-    Board: () => <Portofolio />
+    name: 'Portofolio',
+    Board: () => <Portofolio />,
+    environmentProps: {
+        windowWidth: 1520,
+        windowHeight: 1053,
+        canvasWidth: 1660,
+    },
 });
